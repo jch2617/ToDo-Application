c546f17b963c49ff7b73b4751cbf55e5ee9471f5
jch2617 <jch2617@gmail.com>
2020-01-30 17:24:50 -0500

    fixed compiling error

--- src/app/components/components/todo-item/todo-item.component.ts
+++ src/app/components/components/todo-item/todo-item.component.ts
@@ -8,7 +8,7 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-  @Input() todos: Todo;
+  @Input() todos: Todo[];
   @Input() id: number;
 
 

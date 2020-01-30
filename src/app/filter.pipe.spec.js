c437df4b4c0988de7c46830cb612443a9764db62
jch2617 <jch2617@gmail.com>
2020-01-30 17:05:01 -0500

    final, pushing to git

--- src/app/filter.pipe.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var filter_pipe_1 = require("./filter.pipe");
-describe('FilterPipe', function () {
-    it('create an instance', function () {
-        var pipe = new filter_pipe_1.FilterPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
--- src/app/models/Todo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Todo = /** @class */ (function () {
-    function Todo() {
-    }
-    return Todo;
-}());
-exports.Todo = Todo;
